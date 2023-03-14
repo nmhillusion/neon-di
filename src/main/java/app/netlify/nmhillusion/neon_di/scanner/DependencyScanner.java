08ca639437cb4c097edae58260fab5b4dcbc9ca9
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-03-14 21:56:38 +0700

    fixed scan dependencies

--- src/main/java/app/netlify/nmhillusion/neon_di/scanner/DependencyScanner.java
+++ src/main/java/app/netlify/nmhillusion/neon_di/scanner/DependencyScanner.java
@@ -1,5 +1,6 @@
 package app.netlify.nmhillusion.neon_di.scanner;
 
+import app.netlify.nmhillusion.n2mix.helper.log.LogHelper;
 import app.netlify.nmhillusion.neon_di.exception.NeonException;
 import app.netlify.nmhillusion.neon_di.store.PersistentStore;
 
@@ -9,10 +10,10 @@ import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import static app.netlify.nmhillusion.pi_logger.PiLoggerFactory.getLog;
 
@@ -29,38 +30,76 @@ public class DependencyScanner {
         this.persistentStore = persistentStore;
     }
 
+    public Set<String> getClassNamesFromJarFile(File givenFile) throws IOException {
+        Set<String> classNames = new HashSet<>();
+        try (JarFile jarFile = new JarFile(givenFile)) {
+            Enumeration<JarEntry> e = jarFile.entries();
+            while (e.hasMoreElements()) {
+                JarEntry jarEntry = e.nextElement();
+                if (jarEntry.getName().endsWith(".class")) {
+                    String className = jarEntry.getName()
+                            .replace("/", ".")
+                            .replace(".class", "");
+                    classNames.add(className);
+                }
+            }
+            return classNames;
+        }
+    }
+
     public void scan(Class<?> startClass) throws URISyntaxException, IOException, NeonException, ClassNotFoundException {
+        if (null == startClass) {
+            throw new NeonException("startClass is null");
+        }
+
         final List<Class<?>> classList = new ArrayList<>();
+        final String packageName = startClass.getPackage().getName();
         final URL startLocation = startClass.getProtectionDomain().getCodeSource().getLocation();
         getLog(this).debug("start Neon Engine from start location: " + startLocation);
 
         if (null != startLocation) {
             final List<URL> packageFiles = findPackages(new File(startLocation.toURI()));
             try (URLClassLoader classLoader = new URLClassLoader(packageFiles.toArray(new URL[0]))) {
-                final List<File> classFiles = findClasses(new File(startLocation.toURI()), file -> file.getName().endsWith(".class"));
+                final List<File> classFiles_ = findClasses(new File(startLocation.toURI()), file -> file.getName().endsWith(".class"));
+                final List<String> classNameCollection = new ArrayList<>();
 
-                if (classFiles.isEmpty()) {
-                    throw new NeonException("Resource class files is empty => cannot construct any neon");
+                if (!classFiles_.isEmpty()) {
+                    LogHelper.getLog(this).infoFormat("scan class from directory, size: %s ", classFiles_.size());
+                    classNameCollection.addAll(
+                            classFiles_.stream()
+                                    .map(File::getAbsolutePath)
+                                    .toList()
+                    );
+                } else if (startLocation.getFile().endsWith(".jar") || startLocation.getFile().endsWith(".exe")) {
+                    final Set<String> classNamesFromJarFile = getClassNamesFromJarFile(new File(startLocation.toURI()));
+                    LogHelper.getLog(this).infoFormat("scan class from jar file, size: %s ", classNamesFromJarFile.size());
+                    classNameCollection.addAll(classNamesFromJarFile);
                 }
 
-                for (File clazzFile : classFiles) {
-                    final String packageName = startClass.getPackage().getName();
+                if (classNameCollection.isEmpty()) {
+                    throw new NeonException("Resource class files is empty => cannot construct any neon, from start location: " + startLocation);
+                }
 
-                    String clazzName = clazzFile.getAbsolutePath()
-                            .replaceAll("[\\\\/]", ".");
-                    if (clazzName.contains(".")) {
-                        clazzName = clazzName.substring(0, clazzName.lastIndexOf("."));
-                    }
-
+                for (String clazzName : classNameCollection) {
                     if (clazzName.contains(packageName)) {
-                        clazzName = clazzName.substring(clazzName.indexOf(packageName));
-                    } else {
-                        getLog(this).warn("ignore for class file: " + clazzFile);
-                        break;
-                    }
+                        LogHelper.getLog(this).traceFormat(">> started loading class name: %s", clazzName);
 
-                    final Class<?> aClass = classLoader.loadClass(clazzName);
-                    classList.add(aClass);
+                        clazzName = clazzName
+                                .replaceAll("[\\\\/]", ".");
+                        if (clazzName.contains(".class")) {
+                            clazzName = clazzName.substring(0, clazzName.lastIndexOf(".class"));
+                        }
+
+
+                        clazzName = clazzName.substring(clazzName.indexOf(packageName));
+
+                        LogHelper.getLog(this).traceFormat("<< loaded class name: %s", clazzName);
+
+                        final Class<?> aClass = classLoader.loadClass(clazzName);
+                        classList.add(aClass);
+                    } else {
+                        getLog(this).debug("ignore for class file not in this package: " + clazzName);
+                    }
                 }
             }
         } else {
